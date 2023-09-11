d8a03c7083387e89b63ef48eb8336955bed1bf0d
Emerson Osamah <r.moralesse@gmail.com>
2023-09-11 08:09:29 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
 let track="Frontend";
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const d = new Date();
-document.getElementById("UTCTime").innerHTML = "Day: " + days[d.getDay()];
-document.getElementById("DayOfTheWeek").innerHTML ="Current UTC time in milliseconds: "+ d.getUTCMilliseconds()
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+function timer(){
+    const d = new Date();
+const date = d.getUTCDate();
+const year = d.getUTCFullYear();
+const hours = d.getUTCHours().toString().padStart(2, '0');
+const minutes = d.getUTCMinutes().toString().padStart(2, '0');
+const ms = d.getUTCMilliseconds();
 document.getElementById("Track").innerHTML ="Track: "+ track
+document.getElementById("DayOfTheWeek").innerHTML = "Day: " + days[d.getUTCDay()]+" "+date+" "+ months[d.getUTCMonth()]+", "+year;
+document.getElementById("UTCTime").innerHTML ="Time: " + hours +":"+ minutes + ":" + ms + " UTC";
+}
+
+timer();
+setInterval(timer, 1000);
